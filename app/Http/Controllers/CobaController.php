2e3b46660b303c5123047e6b4d0545d6e6b16c81
muqtafi <ahmadmuqtafi58@gmail.com>
2024-10-26 10:51:28 +0700

    test

--- app/Http/Controllers/CobaController.php
+++ app/Http/Controllers/CobaController.php
@@ -2,12 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Artikel;
 use App\Models\Kategori;
-use App\Models\Komentar;
-use App\Models\Projek;
-use App\Models\Teknologi;
 use App\Models\User;
 
 class CobaController extends Controller{
@@ -15,7 +10,6 @@ class CobaController extends Controller{
     public function index () {
         $user = User::with('projek')->get();
         $kategori = Kategori::with(['artikel', 'artikel.komentar'])->get();
-
         return view('test', compact('user', 'kategori'));
     }
 
--- resources/views/landing-page/about.blade.php
+++ resources/views/landing-page/about.blade.php
@@ -131,7 +131,7 @@
           <div>
             <h3 class="font-semibold text-sm sm:text-base">Rizal</h3>
             <h3 class="font-light text-xs">Member</h3>
-            <p class="font-thin text-xs sm:text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, quis alias? Perspiciatis asperiores recusandae sunt, quasi nihil atque nesciunt natus totam vel sapiente ducimus, ex neque doloribus, ab adipisci iusto.</p>
+            <p class="font-thin text-xs sm:text-sm">We believe in working smart by utilizing time and resources optimally to achieve the best results with efficient effort.</p>
           </div>
         <a>
       </div>
